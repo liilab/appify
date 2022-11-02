1466a51e64a5e9911face04c9a44d69524fdc0c0
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-11-02 19:42:20 +0600

    Token class and method create.

--- includes/User/Token.php
+++ includes/User/Token.php
@@ -4,13 +4,14 @@ namespace WebToApp\User;
 
 class Token
 {
-    public static function get_user_access_token( $user_id, $force_new = false ) {
-		$access_token = get_user_meta( $user_id, 'wta_wc_access_token', true );
-		if ( empty( $access_token ) || $force_new ) {
-			$access_token = 'token_' . wc_rand_hash();
-			update_user_meta( $user_id, 'wta_wc_access_token', $access_token );
-		}
+    public static function get_user_access_token($user_id, $force_new = false)
+    {
+        $access_token = get_user_meta($user_id, 'wta_wc_access_token', true);
+        if (empty($access_token) || $force_new) {
+            $access_token = 'token_' . wc_rand_hash();
+            update_user_meta($user_id, 'wta_wc_access_token', $access_token);
+        }
 
-		return $access_token;
-	}
+        return $access_token;
+    }
 }
