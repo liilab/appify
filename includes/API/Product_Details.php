f42b0106fb16a1550e86b2c379ca2276a504e4c5
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-11-02 19:41:38 +0600

    Token api call.

--- includes/API/Product_Details.php
+++ includes/API/Product_Details.php
@@ -2,11 +2,13 @@
 
 namespace WebToApp\API;
 
-class Product_Details extends \WP_REST_Controller {
+class Product_Details extends \WP_REST_Controller
+{
 	public $user_token;
 	public $user;
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this->namespace = 'web-to-app/v1';
 		$this->rest_base = 'products';
 
@@ -14,73 +16,77 @@ class Product_Details extends \WP_REST_Controller {
 		$this->user_token = \WebToApp\User\Token::get_user_access_token($this->user->ID);
 	}
 
-	public function register_routes() {
-		register_rest_route( $this->namespace, '/' . $this->rest_base, array(
+	public function register_routes()
+	{
+		register_rest_route($this->namespace, '/' . $this->rest_base, array(
 			array(
 				'methods'             => \WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'get_items' ),
-				'permission_callback' => array( $this, 'api_permissions_check' ),
+				'callback'            => array($this, 'get_items'),
+				'permission_callback' => array($this, 'api_permissions_check'),
 				'args'                => $this->get_collection_params(),
 			),
-		) );
+		));
 
-		register_rest_route( $this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)', array(
+		register_rest_route($this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)', array(
 			array(
 				'methods'             => \WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'get_item' ),
-				'permission_callback' => array( $this, 'api_permissions_check' ),
+				'callback'            => array($this, 'get_item'),
+				'permission_callback' => array($this, 'api_permissions_check'),
 				'args'                => array(
-					'context' => $this->get_context_param( array( 'default' => 'view' ) ),
+					'context' => $this->get_context_param(array('default' => 'view')),
 					'id'      => [
-						'description' => __( 'Unique identifier for the object.' ),
+						'description' => __('Unique identifier for the object.'),
 						'type'        => 'integer',
 					],
 				),
 			),
-		) );
+		));
 	}
 
-	public function get_items( $request ) {
+	public function get_items($request)
+	{
 
-		$products = wc_get_products( array(
-			'limit'  => - 1,
+		$products = wc_get_products(array(
+			'limit'  => -1,
 			'page'   => 1,
 			'status' => 'publish',
-		) );
+		));
 
-		$data = array_map( function ( $product ) {
-			return $this->format_wc_product( $product );
-		}, $products );
+		$data = array_map(function ($product) {
+			return $this->format_wc_product($product);
+		}, $products);
 
 
-		$response = new \WP_REST_Response( $data );
-		$response->set_status( 200 );
+		$response = new \WP_REST_Response($data);
+		$response->set_status(200);
 
 		return $response;
 	}
 
-	public function get_item( $request ) {
+	public function get_item($request)
+	{
 		$id   = (int) $request['id'];
-		$post = get_post( $id );
+		$post = get_post($id);
 
-		$product = wc_get_product( $post->ID );
+		$product = wc_get_product($post->ID);
 
-		$products = wc_get_products( array(
-			'limit'  => - 1,
+		$products = wc_get_products(array(
+			'limit'  => -1,
 			'status' => 'publish',
-		) );
+		));
 
 
-		$thumbnail_id = get_post_thumbnail_id( $product->is_type( 'variation' ) ? $product->variation_id : $product->get_id() );
-		$image        = wp_get_attachment_image_src( $thumbnail_id, 'full' );
+		$thumbnail_id = get_post_thumbnail_id($product->is_type('variation') ? $product->variation_id : $product->get_id());
+		$image        = wp_get_attachment_image_src($thumbnail_id, 'full');
 
 
-		return $this->format_wc_product( $product );
+		return $this->format_wc_product($product);
 	}
 
-	public function format_wc_product( \WC_Product $product ): array {
+	public function format_wc_product(\WC_Product $product): array
+	{
 
-		
+
 
 		$data = [
 			'id'                          => $product->get_id(),
@@ -104,11 +110,11 @@ class Product_Details extends \WP_REST_Controller {
 			'downloadable'                => $product->is_downloadable(),
 			'display_add_to_cart'         => $product->is_purchasable() && $product->is_in_stock(),
 			'change_thumbnail_image_size' => 'attention here!',
-			'hide_buy_now_block'          => $product->is_type( 'variable' ),
-//			'buy_now_action'                => $product->is_type( 'variable' ) ? 'select_options' : 'add_to_cart',
+			'hide_buy_now_block'          => $product->is_type('variable'),
+			//			'buy_now_action'                => $product->is_type( 'variable' ) ? 'select_options' : 'add_to_cart',
 			'buy_now_action'              => 'attention here!',
-			'buy_now_button_text'         => $product->is_type( 'variable' ) ? __( 'Select options', 'woocommerce' ) : __( 'Add to cart', 'woocommerce' ),
-			'add_to_cart_button_text'     => __( 'Add to cart', 'woocommerce' ),
+			'buy_now_button_text'         => $product->is_type('variable') ? __('Select options', 'woocommerce') : __('Add to cart', 'woocommerce'),
+			'add_to_cart_button_text'     => __('Add to cart', 'woocommerce'),
 			'qty_config'                  => 'attention here',
 			'stock_quantity'              => $product->get_stock_quantity(),
 			'in_stock'                    => $product->is_in_stock(),
@@ -129,21 +135,20 @@ class Product_Details extends \WP_REST_Controller {
 			'default_attributes'          => 'attention here!',
 			'product_in_webview'          => 'attention here!',
 			'labels'                      => 'attention here!',
-//			'variations'                    => $product->get_available_variations(),
+			//			'variations'                    => $product->get_available_variations(),
 			'variations'                  => $product->get_children(),
 			'product_widgets'             => $product,
 		];
 
 		return $data;
-
 	}
 
-	public function api_permissions_check( $request ) {
-		if ( current_user_can( 'manage_options' ) ) {
+	public function api_permissions_check($request)
+	{
+		if (current_user_can('manage_options')) {
 			return true;
 		}
 
 		return true;
 	}
-
 }
