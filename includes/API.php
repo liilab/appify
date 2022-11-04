a74ef694dc75837ef19080310df8676814dee666
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-11-04 14:36:58 +0600

    Rest api array init.

--- includes/API.php
+++ includes/API.php
@@ -4,14 +4,21 @@ namespace WebToApp;
 
 class API
 {
+    public $routes = array();
+
     public function __construct()
     {
         add_action( 'rest_api_init', [$this, 'register_api'] );
+
+        $this->routes = [ 'Product_Details', 'Create_Order'];
     }
 
     public function register_api()
     {
-        $webtoapp = new API\Product_Details();
-        $webtoapp->register_routes();
+        foreach( $this->routes as $route ) {
+            $class = __NAMESPACE__ . '\\API\\' . $route;
+            $instance = new $class();
+            $instance->register_routes();
+        }
     }
 }
