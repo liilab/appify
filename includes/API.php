d5bc76b669b2461673e68bd11371dbf9c41beb1b
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-11-15 17:16:52 +0600

    Storeinfo route register, cart delete.

--- includes/API.php
+++ includes/API.php
@@ -13,14 +13,14 @@ class API
 
     public function __construct()
     {
-        add_action( 'rest_api_init', [$this, 'register_api'] );
+        add_action('rest_api_init', [$this, 'register_api']);
 
-        $this->routes = [ 'Auth', 'Product', 'Order', 'Address', 'Cart' ];
+        $this->routes = ['Auth', 'Product', 'Order', 'Address', 'StoreInfo'];
     }
 
     public function register_api()
     {
-        foreach( $this->routes as $route ) {
+        foreach ($this->routes as $route) {
             $class = __NAMESPACE__ . '\\API\\' . $route;
             $instance = new $class();
             $instance->register_routes();
