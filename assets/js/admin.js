872df89e28a22fa8d4bee080ba6b87b67ed12e98
asm_wasim <wasim.ve782@gmail.com>
2022-12-13 15:44:16 +0600

    Fixed few api issues

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,66 +1,48 @@
 $ = jQuery;
 
 $(document).ready(function ($) {
+  $("#copy_button").click(function () {
+    $("#access_key").select();
+    document.execCommand("copy");
+    $("#copy_button").text("Copied");
+  });
 
-    $("#copy_button").click(function () {
-        $("#access_key").select();
-        document.execCommand("copy");
-        $("#copy_button").text("Copied");
+  $("#wooapp-getappbtn").click(function () {
+    $("#wooapp-create-app-section").addClass("d-none");
+    $("#wooapp-progressbar").removeClass("d-none");
+
+    var data = {
+      action: "set_post_request",
+    };
+
+    $.ajax({
+      type: "post",
+      url: wta_ajax.admin_ajax,
+      data: data,
+
+      beforeSend: function () {
+        setTimeout(function () {
+          $("#wooapp-progressbar-loader").css("width", "50%");
+        }, 1000);
+      },
+
+      success: function (response) {
+        response = JSON.parse(response);
+
+        console.log(response);
+
+        if (response.status == "SUCCESS") {
+          $("#wooapp-progressbar-loader").css("width", "80%");
+          setTimeout(function () {
+            $("#wooapp-progressbar").addClass("d-none");
+            $("#wooapp-layout").load(location.href + " #wooapp-layout");
+          }, 1500);
+        } else {
+          //show something wrong warning message
+          $("#wooapp-progressbar").addClass("d-none");
+          $("#wooapp-create-app-section").removeClass("d-none");
+        }
+      },
     });
-
-
-
-
-
-
-    $("#wooapp-getappbtn").click(function () {
-        $("#wooapp-getappbtn").text("Loading...");
-        $("#wooapp-progressbar").removeClass("d-none");
-
-        var data = {
-            action: 'set_post_request',
-            // app_name: wta_ajax.app_name,
-            // app_logo: wta_ajax.app_logo,
-            // store_name: wta_ajax.store_name,
-            // store_logo: wta_ajax.store_logo,
-            // base_url: wta_ajax.base_url,
-        };
-
-        var html = "";
-
-        $.ajax({
-            type: 'post',
-            url: wta_ajax.admin_ajax,
-            data: data,
-
-            beforeSend: function () {
-                console.log("ready hosee");
-            },
-
-            success: function (response) {
-                response = JSON.parse(response);
-                console.log(response);
-
-                if (response != "success") {
-                    if (response.pending == false) {
-                        $("#wooapp-progressbar").addClass("d-none");
-
-                        html = ` <div class="wooapp-mail d-flex justify-content-between">
-                <input type="email" class="form-control" placeholder=" `+ response.binary + `">
-                <button id="wooapp-downloadappbtn" class="btn btn-primary text-center">Send Download Link</button>
-            </div>`;
-
-                       // $("#wooapp-getappbtn").text("Download App");
-                        //$("#wooapp").append(html);
-                        $("#wooapp-layout").load(location.href + " #wooapp-layout") //reload the div;
-                    }
-                } else {
-                    $("#wooapp-progressbar").addClass("d-none");
-                    $("#wooapp-getappbtn").text("Error!");
-                }
-
-            },
-        });
-
-    });
+  });
 });
--- includes/Admin/Backend.php
+++ includes/Admin/Backend.php
@@ -13,112 +13,86 @@ class Backend
         $this->hooks();
     }
 
-
     public function hooks()
     {
         add_action('wp_ajax_set_post_request', [$this, 'set_post_request']);
     }
 
-
     public function set_post_request()
     {
 
+        $dummy_logo = 'https://play-lh.googleusercontent.com/BUB9hjJqtkBjHekgrqsINgzNMzA-G34nyZQDRmzmQdw6_qbpO8E9l78Z9wS0eCp8QFKE';
+
+        $base_url = 'http://192.168.0.129:8000/';
+
         $token = 'Token f6fbdfbff8f79e03c80048f8c8daf1906a9c3bb4';
 
-        $url = 'http://192.168.0.129:8000/api/builder/v1/create-build-request/';
+        $url = $base_url . 'api/builder/v1/create-build-request/';
 
         $user_id = $this->get_current_user_id();
         $user_info = get_userdata($user_id);
-        $user_id = 1111;
+        $user_id = 1008;
 
-       
+        // $previous_build_url = get_user_meta($user_id, 'build_url', true);
+        $build_id = get_user_meta($user_id, 'build_id', true);
 
-        $is_url_exist = get_user_meta($user_id, 'build_url', true);
+        if (empty($build_id)) {
+            $args = array(
+                'headers' => array(
+                    'Authorization' => $token,
+                ),
 
-        if (!empty($is_url_exist)) {
-            $send = [
-                "ok" => true,
-                "pending" => false,
-                "status" => true,
-                "binary" => $is_url_exist,
-            ];
+                'body' => array(
+                    'first_name' => empty($user_info->first_name) ? '--' : $user_info->first_name,
+                    'last_name' => empty($user_info->last_name) ? '--' : $user_info->last_name,
+                    'username' => $user_info->user_login,
+                    'email' => $user_info->user_email,
+                    'app_name' => get_bloginfo('name'),
+                    'app_logo' => $dummy_logo,
+                    'store_name' => get_bloginfo('name'),
+                    'store_logo' => $dummy_logo,
+                    'domain' => get_bloginfo('url'),
+                    "template" => 1,
+                ),
+            );
 
-
-            echo json_encode($send);
-            wp_die();
-        }
-
-
-
-        $args = array(
-            'headers' => array(
-                'Authorization' => $token,
-            ),
-
-
-
-            'body' => array(
-                'first_name' => $user_info->first_name,
-                'last_name' => $user_info->last_name,
-                'username' => $user_info->user_login,
-                'email' => $user_info->user_email,
-                'app_name' => get_bloginfo('name'),
-                'app_logo' => 'https://play-lh.googleusercontent.com/BUB9hjJqtkBjHekgrqsINgzNMzA-G34nyZQDRmzmQdw6_qbpO8E9l78Z9wS0eCp8QFKE',
-                'store_name' => get_bloginfo('name'),
-                'store_logo' => 'https://play-lh.googleusercontent.com/BUB9hjJqtkBjHekgrqsINgzNMzA-G34nyZQDRmzmQdw6_qbpO8E9l78Z9wS0eCp8QFKE',
-                'domain' => get_bloginfo('url'),
-                "template" => 1,
-
-
-                // "first_name" => "Abir",
-                // "last_name" =>  "Wasim",
-                // "username" => "sabbir7123",
-                // "email" => "sabbir7123@gmail.com",
-                // "app_name" => "Shatkora",
-                // "app_logo" => "https://play-lh.googleusercontent.com/BUB9hjJqtkBjHekgrqsINgzNMzA-G34nyZQDRmzmQdw6_qbpO8E9l78Z9wS0eCp8QFKE",
-                // "store_name" => "Shatkora",
-                // "store_logo" => "https://play-lh.googleusercontent.com/BUB9hjJqtkBjHekgrqsINgzNMzA-G34nyZQDRmzmQdw6_qbpO8E9l78Z9wS0eCp8QFKE",
-                // "domain" => "https://liipress.abirwasim.me",
-                // "template" => 1
-            )
-        );
-
-        // echo json_encode($args['body']);
-
-        $is_request_create_previously = get_user_meta($user_id, 'build_id', true);
-
-
-        if (empty($is_request_create_previously)) {
             $response = wp_remote_post($url, $args);
 
             $body = $response['body'];
 
-
             $body =  json_decode($body, true);
 
-            $is_pending = empty($body['message']) ? false : true;
+            $is_pending_build_request = empty($body['message']) ? false : true;
 
-            if ($is_pending == false) {
+            if ($is_pending_build_request == false) {
                 $build_id =  $body['id'];
                 update_user_meta($user_id, 'build_id', $build_id);
             }
-        } else {
-            $build_id = $is_request_create_previously;
-            $is_pending = false;
         }
 
-        if ($is_pending == false) {
+        // NOT_BUILT = 'NOT_BUILT', _('Not Built')
+        // UNSTABLE = 'UNSTABLE', _('Unstable')
+        // SUCCESS = 'SUCCESS', _('Success')
+        // FAILURE = 'FAILURE', _('Failure')
+        // ABORTED = 'ABORTED', _('Aborted')
+
+        $build_response = array(
+            "status" => 'NOT_BUILT',
+            "binary" => '',
+        );
+
+        if (!empty($build_id)) {
             $args = array(
                 'headers' => array(
                     'Authorization' => $token,
                 ),
             );
 
-            $is_loop = true;
+            $request_cnt = 0;
 
-            while ($is_loop) {
+            while (true) {
 
-                $url = 'http://192.168.0.129:8000//api/builder/v1/build-request/' . $build_id . '/';
+                $url = $base_url . 'api/builder/v1/build-request/' . $build_id . '/';
 
                 $response = wp_remote_get($url, $args);
 
@@ -127,23 +101,29 @@ class Backend
 
                 $status = $body['status'];
 
-                if ($status === "SUCCESS") {
+                if ($status !== "NOT_BUILT") {
                     update_user_meta($user_id, 'build_url', $body['binary']);
-                    $send = [
-                        "pending" => false,
-                        "status" => true,
+                    $build_response = [
+                        "status" => $status,
                         "binary" => $body['binary'],
                     ];
-
                     break;
                 }
 
-                sleep(5);
+                $request_cnt++;
+                if ($request_cnt >= 20) {
+                    $build_response = [
+                        "status" => $status,
+                        "binary" => $body['binary'],
+                    ];
+                    break;
+                }
+
+                sleep(30);
             }
         }
 
-
-        echo json_encode($send);
+        echo json_encode($build_response);
         wp_die();
     }
 
