bba53159cf7453be42ee163d06f6ee6c59726bb5
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-12-17 18:02:30 +0600

    Format document

--- includes/Admin/Admin_Page.php
+++ includes/Admin/Admin_Page.php
@@ -197,7 +197,11 @@ class Admin_Page
                     }
                 }
                 printf(
-                    '<input style="display:none;" id="%s" name="%s" type="text" value="%s"  data-return="%s"><div id="preview%s" style="margin-right:10px;border:1px solid #e2e4e7;background-color:#fafafa;display:inline-block;width: 100px;height:100px;background-image:url(%s);background-size:cover;background-repeat:no-repeat;background-position:center;"></div><input style="width: 19%%;margin-right:5px;" class="button menutitle-media" id="%s_button" name="%s_button" type="button" value="Select" /><input style="width: 19%%;" class="button remove-media" id="%s_buttonremove" name="%s_buttonremove" type="button" value="Clear" />',
+                    '<input style="display:none;" id="%s" name="%s" type="text" value="%s"  data-return="%s">
+                    <div id="preview%s" style="margin-right:10px;border:1px solid #e2e4e7;background-color:#fafafa;display:inline-block;width: 100px;height:100px;background-image:url(%s);background-size:cover;background-repeat:no-repeat;background-position:center;">
+                    </div>
+                    <input style="width: 19%%;margin-right:5px;" class="button menutitle-media" id="%s_button" name="%s_button" type="button" value="Select" />
+                    <input style="width: 19%%;" class="button remove-media" id="%s_buttonremove" name="%s_buttonremove" type="button" value="Clear" />',
                     $field['id'],
                     $field['id'],
                     $value,
