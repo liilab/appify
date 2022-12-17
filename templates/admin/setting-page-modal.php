a9964a6554e2462b02a99e34c5adfbafb857e526
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-12-17 17:48:12 +0600

    Modal area create in setting page

--- templates/admin/setting-page-modal.php
+++ templates/admin/setting-page-modal.php
@@ -1,8 +1,8 @@
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-dialog modal-dialog-centered modal-lg  modal-dialog-scrollable">
         <div class="modal-content">
-            <div class="modal-header">
+            <div class="modal-header mt-4 mt-md-0">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Give your information</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
@@ -13,7 +13,8 @@
                         <?php
                         settings_fields('wta_custom');
                         do_settings_sections('wta_custom');
-                        submit_button();
+                        $other_attributes = array( 'id' => 'wta-save-appinfo' );
+                        submit_button( __( 'Save Info', 'wta' ), 'primary', 'wta-save-appinfo', true, $other_attributes );
                         ?>
                     </form>
                 </div>
