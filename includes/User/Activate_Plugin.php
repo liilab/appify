6c2d67c0779b58dca988c680d128ba849504ae23
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-12-17 11:37:12 +0600

    Activate plugin class create.

---
+++ includes/User/Activate_Plugin.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace WebToApp\User;
+
+class Activate_Plugin
+{
+
+    public function __construct()
+    {
+        add_action('user_register', [$this, 'wta_registration_save']);
+    }
+
+    public function wta_registration_save($user_id)
+    {
+        $this->create_user_token($user_id);
+    }
+
+
+    private function create_user_token($user_id, $force_new = false)
+    {
+
+        $url = 'http://192.168.0.129:8000/api/builder/v1/activate-plugin/';
+
+
+
+        $user_info = get_userdata($user_id);
+
+        $data = array(
+            'user' => array(
+                'first_name' => $user_info->first_name? $user_info->first_name : '',
+                'last_name' => $user_info->last_name? $user_info->last_name : '',
+                'email' => $user_info->user_email? $user_info->user_email : '',
+            ),
+            'website' => array(
+                'name' => 'Lii Store',
+                'domain' => 'https://liipress.abirwasim.me',
+            ),
+            'keystore' => array(
+                'city' => 'Sylhet',
+                'country' => 'BD',
+                'name' => 'Abir Sadat',
+                'state' => 'Sylhet',
+                'organization' => 'Lii Store',
+                'organizational_unit' => 'lii-store-e-commerce',
+            ),
+        );
+
+        $response = $this->create_plugin_activate_post_request($url, $data);
+
+        if (is_wp_error($response)) {
+            $error_message = $response->get_error_message();
+            echo "Something went wrong: $error_message";
+        } else {
+            $this->save_user_data($response);
+        }
+    }
+
+    public function create_plugin_activate_post_request($url, $data)
+    {
+        $args = array(
+            'method' => 'POST',
+            'body' => $data,
+        );
+        $response = wp_remote_post($url, $args);
+        return $response;
+    }
+
+    public function save_user_data($response){
+        $token = $response['token'];
+        $user_id = $response['user_id'];
+        $website_id = $response['website_id'];
+
+        $id = $this->get_current_user_id();
+
+        update_user_meta($id, 'wta_wc_user_id', $user_id);
+        update_user_meta($id, 'wta_wc_access_token', $token);
+        update_user_meta($id, 'wta_wc_website_id', $website_id);
+    }
+
+    public function get_current_user_id(): int
+    {
+        $current_user = wp_get_current_user();
+        return $current_user->ID;
+    }
+}
