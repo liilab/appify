1a40e75b375ead204718951b186c15c1dfad0288
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-12-27 12:16:23 +0600

    Remove special charachter method create.

--- includes/WtaHelper.php
+++ includes/WtaHelper.php
@@ -10,6 +10,17 @@ class WtaHelper
     use Singleton;
 
     /**
+     * String Cleaner
+     */
+
+     public static function clean($string) {
+        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+        $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+     
+        return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.
+     }
+
+    /**
      * return the current user id
      * @return int
      */
